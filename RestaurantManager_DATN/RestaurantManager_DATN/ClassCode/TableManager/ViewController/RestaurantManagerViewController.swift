4e0f5e2564423957cbf02538519cd3ad078fa4c6
Tuấn Anh <>
2023-03-17 00:19:54 +0700

    add font

---
+++ RestaurantManager_DATN/RestaurantManager_DATN/ClassCode/TableManager/ViewController/RestaurantManagerViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RestaurantManagerViewController.swift
+//  RestaurantManager_DATN
+//
+//  Created by Phạm Tuấn Anh on 16/03/2023.
+//  Copyright © 2023 Hoang Dinh Huy. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantManagerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
