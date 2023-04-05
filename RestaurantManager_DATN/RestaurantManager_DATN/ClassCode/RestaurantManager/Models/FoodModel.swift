7c0a7c5bc32b97911601d9e343a899f873a6b75d
Tuấn Anh <>
2023-04-05 23:08:25 +0700

    fix model

--- RestaurantManager_DATN/RestaurantManager_DATN/ClassCode/RestaurantManager/Models/FoodModel.swift
+++ RestaurantManager_DATN/RestaurantManager_DATN/ClassCode/RestaurantManager/Models/FoodModel.swift
@@ -11,8 +11,7 @@ import ObjectMapper
 
 struct FoodModel: Decodable, Mappable {
     
-    init?(map: ObjectMapper.Map) {
-    }
+    
     
     var foodId: String! = UUID().uuidString
     var foodName: String? = ""
@@ -24,7 +23,115 @@ struct FoodModel: Decodable, Mappable {
     var inBill: Int? = -1
     var didDelete: Int? = 0
     
+    static func fetchMenuData(completion: @escaping ([FoodModel]?, Error?) -> Void) {
+        let db = Firestore.firestore()
+        var datas = [FoodModel]()
+
+        db.collection("MonAn").whereField("daxoa", isEqualTo: 0).whereField("trongthucdon", isEqualTo: 1).order(by: "tenmonan").getDocuments { (snapshot, err) in
+            if err != nil {
+                
+                print("Error getting BanAn Data: \(err!.localizedDescription)")
+                completion(nil, err)
+                
+            } else if let snapshot = snapshot, !snapshot.documents.isEmpty {
+                
+                snapshot.documents.forEach({ (document) in
+                    if let data = FoodModel(JSON: document.data()) {
+                        datas.append(data)
+                    }
+                })
+                completion(datas, nil)
+            } else {
+                completion(datas, nil)
+            }
+        }
+    }
+    
+    static func fetchAllDataAvailable(completion: @escaping ([FoodModel]?, Error?) -> Void) {
+        let db = Firestore.firestore()
+        var datas = [FoodModel]()
+
+        db.collection("MonAn").whereField("daxoa", isEqualTo: 0).order(by: "tenmonan").getDocuments { (snapshot, err) in
+            if err != nil {
+                
+                print("Error getting BanAn Data: \(err!.localizedDescription)")
+                completion(nil, err)
+                
+            } else if let snapshot = snapshot, !snapshot.documents.isEmpty {
+                
+                snapshot.documents.forEach({ (document) in
+                    if let data = FoodModel(JSON: document.data()) {
+                        datas.append(data)
+                    }
+                })
+                completion(datas, nil)
+            } else {
+                completion(datas, nil)
+            }
+        }
+    }
+    
+    static func fetchAllData(completion: @escaping ([FoodModel]?, Error?) -> Void) {
+        let db = Firestore.firestore()
+        var datas = [FoodModel]()
+
+        db.collection("MonAn").order(by: "tenmonan").getDocuments { (snapshot, err) in
+            if err != nil {
+                
+                print("Error getting BanAn Data: \(err!.localizedDescription)")
+                completion(nil, err)
+                
+            } else if let snapshot = snapshot, !snapshot.documents.isEmpty {
+                
+                snapshot.documents.forEach({ (document) in
+                    if let data = FoodModel(JSON: document.data()) {
+                        datas.append(data)
+                    }
+                })
+                completion(datas, nil)
+            } else {
+                completion(datas, nil)
+            }
+        }
+    }
+
+    static func fetchData(byOrder order: OrdersModel, completion: @escaping (FoodModel?, Error?) -> Void) {
+        let db = Firestore.firestore()
+        guard let idmonan = order.foodId else {
+            return
+        }
+        db.collection("MonAn").document(idmonan).getDocument { (snapshot, err) in
+            if err != nil {
+                
+                print("Error getting BanAn Data: \(err!.localizedDescription)")
+                completion(nil, err)
+                
+            } else if let data = snapshot?.data() {
+                
+                let dish = FoodModel(JSON: data)
+
+                completion(dish, nil)
+            } else {
+                completion(FoodModel(), nil)
+            }
+        }
+    }
+    
+    func updateInMenu(forDish dish: FoodModel, completion: @escaping (Error?) -> Void) {
+        let db = Firestore.firestore()
+
+        db.collection("MonAn").document(dish.foodId).updateData([
+            "trongthucdon": dish.inBill,
+        ]) { err in
+            completion(err)
+        }
+        completion(nil)
+    }
+    
     init?() {
+    }
+    
+    init?(map: ObjectMapper.Map) {
     }
     
     mutating func mapping(map: Map) {
@@ -42,7 +149,6 @@ struct FoodModel: Decodable, Mappable {
     func hash(into hasher: inout Hasher) {
         hasher.combine(foodId)
     }
-
     
     static func == (lhs: FoodModel, rhs: FoodModel) -> Bool {
         return lhs.foodId == rhs.foodId
--- RestaurantManager_DATN/RestaurantManager_DATN/ClassCode/RestaurantManager/Models/TableModel.swift
+++ RestaurantManager_DATN/RestaurantManager_DATN/ClassCode/RestaurantManager/Models/TableModel.swift
@@ -15,11 +15,83 @@ struct TableModel: Decodable, Mappable {
     var numberOrdinalTable: String? = ""
     var numberOfChair: Int? = -1
     var didDelete: Int? = 0
-    
     var bill: BillModel?
     
     static func == (lhs: TableModel, rhs: TableModel) -> Bool {
         return lhs.tableId == rhs.tableId
+    }
+    
+    static func fetchAllDataAvailable(completion: @escaping ([TableModel]?, Error?) -> Void) {
+        var banans = [TableModel]()
+        let db = Firestore.firestore()
+
+        db.collection("BanAn").whereField("daxoa", isEqualTo: 0).order(by: "sobanan").getDocuments { (snapshot, err) in
+            if err != nil {
+                
+                print("Error getting BanAn Data: \(err!.localizedDescription)")
+                completion(nil, err)
+                
+            } else if snapshot != nil, !snapshot!.documents.isEmpty {
+                
+                snapshot!.documents.forEach({ (document) in
+                    if let banan = TableModel(JSON: document.data()) {
+                        banans.append(banan)
+                    }
+                })
+                completion(banans, nil)
+            } else {
+                completion(banans, nil)
+            }
+        }
+    }
+    
+    static func fetchAllData(completion: @escaping ([FoodModel]?, Error?) -> Void) {
+        var banans = [FoodModel]()
+        let db = Firestore.firestore()
+
+        db.collection("BanAn").order(by: "sobanan").getDocuments { (snapshot, err) in
+            if err != nil {
+                
+                print("Error getting BanAn Data: \(err!.localizedDescription)")
+                completion(nil, err)
+                
+            } else if snapshot != nil, !snapshot!.documents.isEmpty {
+                
+                snapshot!.documents.forEach({ (document) in
+                    if let banan = FoodModel(JSON: document.data()) {
+                        banans.append(banan)
+                    }
+                })
+                completion(banans, nil)
+            } else {
+                completion(banans, nil)
+            }
+        }
+    }
+    
+    static func fetchData(ofID id: String, completion: @escaping (FoodModel?, Error?) -> Void) {
+        if id == "" {
+            completion(nil,nil)
+            return
+        }
+        var banan: FoodModel?
+        let db = Firestore.firestore()
+
+        db.collection("BanAn").document(id).getDocument { (snapshot, err) in
+            if err != nil {
+                
+                print("Error getting BanAn Data: \(err!.localizedDescription)")
+                completion(nil, err)
+                
+            } else if snapshot != nil, snapshot?.data() != nil {
+                
+                banan = FoodModel(JSON: snapshot!.data()!)
+                completion(banan, nil)
+                
+            } else {
+                completion(banan, nil)
+            }
+        }
     }
     
     func hash(into hasher: inout Hasher) {
